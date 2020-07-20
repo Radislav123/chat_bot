70eb7f3e43699369a0cc67402f32f6e3d75699a1
rvvlasov <radislavvlasov@gmail.com>
2020-07-20 16:11:40 +0700

    flask test

---
+++ src/flask_test.py
@@ -0,0 +1,14 @@
+import flask
+
+# www.simplebot.ru
+IP = "18.218.144.4"
+app = flask.Flask(__name__)
+
+
+# Empty webserver index, return nothing, just http 200
+@app.route('/')
+def index():
+	return 'Hello, world!'
+
+
+app.run(host = "0.0.0.", port = 8443, debug = True)
