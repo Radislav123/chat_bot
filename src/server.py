819c972103493a4549649c6b285d34e178a99cd8
rvvlasov <radislavvlasov@gmail.com>
2020-07-20 17:04:18 +0700

    tries with webhook 1.0.22

--- src/server.py
+++ src/server.py
@@ -63,4 +63,4 @@ if __name__ == '__main__':
 	bot.set_webhook(url = WEBHOOK_URL_BASE + WEBHOOK_URL_PATH, certificate = open(WEBHOOK_SSL_CERT, 'r'))
 
 	# Start flask server
-	app.run(host = WEBHOOK_LISTEN, port = WEBHOOK_PORT, ssl_context = (WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
+	app.run(host = WEBHOOK_LISTEN, port = WEBHOOK_PORT, debug = True, ssl_context = (WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV))
