88ba0fbc90d67f6aaea97fc77775b72749e85d99
rvvlasov <radislavvlasov@gmail.com>
2020-07-20 16:14:06 +0000

    is it working?

--- src/server.py
+++ src/server.py
@@ -7,12 +7,16 @@ API_TOKEN = "933175966:AAENp5e-3y2DzknBhNPQZ_HAzerkbjX-a1E"
 # www.simplebot.ru
 IP = "18.218.144.4"
 DOMAIN = "ec2-18-218-144-4.us-east-2.compute.amazonaws.com"
-WEBHOOK_HOST = DOMAIN
+WEBHOOK_HOST = IP
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = "0.0.0.0"  # In some VPS you may need to put here the IP addr
 
+# WEBHOOK_SSL_CERT = '../attachments/YOURPUBLIC.pem'  # Path to the ssl certificate
+# WEBHOOK_SSL_PRIV = '../attachments/YOURPRIVATE.key'  # Path to the ssl private key
+
 WEBHOOK_SSL_CERT = '../attachments/webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = '../attachments/webhook_pkey.pem'  # Path to the ssl private key
+
 
 WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
 WEBHOOK_URL_PATH = "/%s/" % API_TOKEN
