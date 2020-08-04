3e85e7ec30752c05ec2dba51d976f60fc5de198b
rvvlasov <radislavvlasov@gmail.com>
2020-08-04 16:55:18 +0300

    trying to get public ip

--- src/try.py
+++ src/try.py
@@ -20,11 +20,7 @@ def try_urls():
 
 
 def get_ip():
-	import socket
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	s.connect(("8.8.8.8", 80))
-	print(s.getsockname()[0])
-	s.close()
+	return requests.get('https://api.ipify.org').text
 
 
-get_ip()
+print(get_ip())
